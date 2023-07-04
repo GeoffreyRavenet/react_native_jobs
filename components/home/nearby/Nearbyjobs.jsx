e21b44f0e9f12ff3c25486d29ccbff0feea2a885
GeoffreyRavenet <ravenet.geoffreypro@gmail.com>
2023-07-04 12:23:01 +0000

    ✨ NearbyJob > add api + card

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+
+import { View, Text, TouchableOpacity, ActivityIndicator } from 'react-native'
 
 import styles from './nearbyjobs.style'
 
+import { COLORS, SIZES } from '../../../constants'
+import NearbyJobCard from '../../common/cards/nearby/NearbyJobCard'
+import { useRouter } from 'expo-router'
+import useFetch from '../../../hook/useFetch'
+
+
 const Nearbyjobs = () => {
+  const router = useRouter()
+  const {data, isLoading, error} = useFetch('search',
+   {
+    query:  'React developer in paris',
+    num_pages: '1'
+  })
+
   return (
-    <View>
-      <Text>Nearbyjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Nearby jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.cardsContainer}>Tout afficher</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" color={COLORS.primary} />
+        ) : error ? (
+          <Text>Quelque chose s'est mal passé</Text>
+        ) : (
+          data?.map((job) => (
+            <NearbyJobCard 
+              job={job}
+              key={`nearby-job-${job?.job_id}`}
+              handleNavigate={() => router.push(`/job-details/${job?.job_id}`)}
+            />
+          ))
+        )
+      
+      }
+      </View>
     </View>
   )
 }
